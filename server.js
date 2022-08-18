5513aa912f7eb6f904f165333a28dcda4ea14170
AndresggDev <andgg.dev@gmail.com>
2022-08-18 15:38:27 -0500

    commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/mi-cartilla'));
+app.use(express.static('./dist/cartilla'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/mi-cartilla/index.html' })
+    res.sendFile('index.html', {root: 'dist/cartilla/index.html' })
 );
 
 // Start the app by listening on the default Heroku port
